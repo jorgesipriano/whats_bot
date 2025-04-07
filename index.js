715460eb5105af846d4729f6be9b7f716d48de99
jorge_sipriano <154374041+jorgesipriano@users.noreply.github.com>
2025-04-07 15:29:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,10 @@ async function addPedidoAoSheet(pedidoData) {
 }
 
 const client = new Client({
-  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
 });
 
 client.on('qr', (qr) => {
