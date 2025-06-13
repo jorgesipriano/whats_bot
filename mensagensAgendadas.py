550281663b0ae0b8e58a5322c772aed7ef367cb3
jorge_sipriano <154374041+jorgesipriano@users.noreply.github.com>
2025-06-13 14:28:51 -0300

    Update mensagensAgendadas.py

--- mensagensAgendadas.py
+++ mensagensAgendadas.py
@@ -1,41 +1,35 @@
 import requests
 
 def enviar_culto_quarta():
-    message = "Lembrete: Culto de Quarta às 20h! Já enviou sua confirmação?!"
+    message = "Lembrete: Já enviou sua confirmação?!"
     response = requests.post('http://localhost:3000/enviar-mensagem', json={'mensagem': message})
     return response.status_code == 200
 
 def enviar_lembrete_terca():
     message = """
-*Culto Quarta*
-
-Débora 
-Jorge 
-Nael
-Pra. Priscila 
-Warlen
-
-Sugestões:
+*mensagem confirmação*
+#
+#
+#
+#
+**** 
 """
     response = requests.post('http://localhost:3000/enviar-mensagem', json={'mensagem': message})
     return response.status_code == 200
 
 def enviar_culto_domingo():
-    message = "Lembrete: Culto de Domingo já indicou as músicas, está tudo certo pro culto né?"
+    message = "Lembrete: Já enviou sua confirmação?!"
     response = requests.post('http://localhost:3000/enviar-mensagem', json={'mensagem': message})
     return response.status_code == 200
 
 def enviar_lembrete_sexta():
     message = """
-*Culto Domingo*
-
-Débora 
-Jorge 
-Nael
-Pra. Priscila 
-Warlen
-
-Sugestões:
+*mensagem confirmação* 
+#
+#
+#
+#
+**** 
 """
     response = requests.post('http://localhost:3000/enviar-mensagem', json={'mensagem': message})
     return response.status_code == 200
